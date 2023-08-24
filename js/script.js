d134ca266ef53400a554803a7650f9fcdcbbd05f
Ahmed Helmy <42387111+AhmddHelmy@users.noreply.github.com>
2023-08-24 13:34:23 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+"use strict"
+
+class TxtType {
+    constructor(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    }
+    tick() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+            this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+            this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 2; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+            delta = this.period;
+            this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+            this.isDeleting = false;
+            this.loopNum++;
+            delta = 500;
+        }
+
+        setTimeout(function () {
+            that.tick();
+        }, delta);
+    }
+}
+
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('typewrite');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-type');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+    document.body.appendChild(css);
+};
+
+// // Change Nav color: 
+
+// $(window).scroll(function () {
+//     var scrollPosition = $(window).scrollTop();
+//     console.log(scrollPosition);
+//     if (scrollPosition > 50) {
+//         $(".navbar").css("background-color","red");
+//     } else{
+//         $(".navbar").css("background-color","transparent");
+//     }
+// });
+
+
+window.addEventListener("scroll", function () {
+    var nav = document.querySelector(".navbar");
+    console.log(nav.style.backgroundColor);
+    if(window.scrollY > 50){
+        nav.style.setProperty("background-color", "#1e1e1e", "important");
+    }
+    else{
+        nav.style.setProperty("background-color", "transparent", "important");
+    }
+});
